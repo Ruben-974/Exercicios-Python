e491af64a0878113054c4bc70b71f06ca1d87a9c
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2021-10-06 15:43:02 -0300

    Initial commit

--- Mundo 3/Aula 23 - Tratamento de erros e exceções/Ex113 - Funções aprofundadas em python/Programa.py
+++ Mundo 3/Aula 23 - Tratamento de erros e exceções/Ex113 - Funções aprofundadas em python/Programa.py
@@ -1,5 +1,5 @@
 from FunçõesLeia import leiaint, leiafloat
 
-inteiro = leiaint('Diggite um valor inteiro: ')
-real = leiafloat('Digigte um valor real: ')
+inteiro = leiaint('Digite um valor inteiro: ')
+real = leiafloat('Digite um valor real: ')
 print(f'O valor inteiro digitado foi {inteiro} e o valor real foi {real}')
